5647353e3b1e62e51d6ab909821a8aec1c5fdfdd
rhkts <tahagoo168@gmail.com>
2024-07-20 17:21:05 +0900

    5-02 プルダウンメニューで指定ページへ

--- 確かな力が身につくJavaScript超入門/work/5-02_location/script.js
+++ 確かな力が身につくJavaScript超入門/work/5-02_location/script.js
@@ -1,5 +1,20 @@
-'use strict';
+"use strict";
 
-document.getElementById('form').select.onchange = function () {
-    location.href = document.getElementById('form').select.value;
-}
+const lang = document.querySelector("html").lang;
+
+// if (lang === "ja") {
+//   //ここでHTMLの<html></html>を取得
+//   //CSSでいうとタイプセレクタ
+//   //<option></option>タグのうち、value属性が、"index.html"であるものがマッチする→<option value="index.html">日本語</option>
+//   document.querySelector('option[value="index.html"').selected = true;
+// } else if (lang === "en") {
+//   document.querySelector('option[value="index-en.html"').selected = true;
+// } else if (lang === "zh") {
+//   document.querySelector('option[value="index-zh.html"').selected = true;
+// }
+
+//onChangeイベントが発生するのは、今と違う<option></option>が選択されあた時だけ。
+//日本語→日本語はonChangeイベントは発生しない
+document.getElementById("form").select.onchange = function () {
+  location.href = document.getElementById("form").select.value;
+};
