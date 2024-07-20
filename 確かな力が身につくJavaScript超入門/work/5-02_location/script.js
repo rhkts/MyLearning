3870c523fe1a89062e49331ab010d878494197e7
rhkts <tahagoo168@gmail.com>
2024-07-20 17:22:19 +0900

    5-02 余計なところまでコメントにしていたため修正

--- 確かな力が身につくJavaScript超入門/work/5-02_location/script.js
+++ 確かな力が身につくJavaScript超入門/work/5-02_location/script.js
@@ -2,16 +2,17 @@
 
 const lang = document.querySelector("html").lang;
 
-// if (lang === "ja") {
-//   //ここでHTMLの<html></html>を取得
-//   //CSSでいうとタイプセレクタ
-//   //<option></option>タグのうち、value属性が、"index.html"であるものがマッチする→<option value="index.html">日本語</option>
-//   document.querySelector('option[value="index.html"').selected = true;
-// } else if (lang === "en") {
-//   document.querySelector('option[value="index-en.html"').selected = true;
-// } else if (lang === "zh") {
-//   document.querySelector('option[value="index-zh.html"').selected = true;
-// }
+//ifを使った例
+if (lang === "ja") {
+  //ここでHTMLの<html></html>を取得
+  //CSSでいうとタイプセレクタ
+  //<option></option>タグのうち、value属性が、"index.html"であるものがマッチする→<option value="index.html">日本語</option>
+  document.querySelector('option[value="index.html"').selected = true;
+} else if (lang === "en") {
+  document.querySelector('option[value="index-en.html"').selected = true;
+} else if (lang === "zh") {
+  document.querySelector('option[value="index-zh.html"').selected = true;
+}
 
 //onChangeイベントが発生するのは、今と違う<option></option>が選択されあた時だけ。
 //日本語→日本語はonChangeイベントは発生しない
