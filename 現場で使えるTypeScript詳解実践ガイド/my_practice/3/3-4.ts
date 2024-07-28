219db84a331ec6fad1a9ffcdb6e8367dcf4a08d1
rhkts <tahagoo168@gmail.com>
2024-07-28 09:48:17 +0900

    3-4 型エイリアス追加

---
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/3/3-4.ts
@@ -0,0 +1,16 @@
+//3-12 型エイリアス
+type Role = number | string; //number型とstring型のユニオン型にRoleという名前をつける
+type EventType = "click" | "hover" | "keydown"; //リテラル型のユニオン型にEventTypeという名前をつける
+
+//3-13 型注釈による型エイリアスの利用
+let firstRole: Role; //Roleを指定
+let lastRole: Role; //Role型を使いまわす
+
+//3-14 型エイリアスのユニオン型
+type Animal = Cat | Dog; //Animalエイリアスに、Cat型とDog型を指定
+
+//後から Cat型 と Dog型 を指定してもOK
+type Cat = "mike" | "dora" | "pug";
+type Dog = "shiba" | "poodle" | "pug";
+
+let pet: Animal = "shiba"; //
