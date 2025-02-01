c47c7b838539728d799c2f938b3e27601e053f4d
rhkts <tahagoo168@gmail.com>
2025-02-01 15:48:03 +0900

    MUIのダイアログサンプルを追加

---
+++ my-component-viewer/src/components/mui/MuiDialog.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+
+const MuiDialog = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <div>
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        ダイアログを開く
+      </Button>
+
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>ダイアログのタイトル</DialogTitle>
+        <DialogContent>ダイアログの内容</DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>ダイアログを閉じる</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default MuiDialog;
