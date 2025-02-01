781d449c0ce32cfdaceab452bed0bf50d23a01b0
rhkts <tahagoo168@gmail.com>
2025-02-01 14:13:16 +0900

    MUIも追加(サンプルコードのため)

--- my-component-viewer/src/App.tsx
+++ my-component-viewer/src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "@mui/material/Button";
+
 function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <Button variant="contained" color="primary" className="mt-4">
+        MUI Button
+      </Button>
     </div>
   );
 }
