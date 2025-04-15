f1853e2cd9f5b28b66bc5602fbed11c79b5fb188
rhkts <tahagoo168@gmail.com>
2025-04-15 11:01:33 +0900

    6-4 振り返り

--- 現場で使えるTypeScript詳解実践ガイド/my_practice/6/振り返り.ts
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/6/振り返り.ts
@@ -1,39 +1,122 @@
-class LimitedStorage<T extends string | number> {
+class ColorStorage<T> {
+  private colos: T[] = [];
+
+  addColor(color: T): void {
+    this.colos.push(color);
+  }
+
+  getAllColors(): T[] {
+    return [...this.colos];
+  }
+}
+
+let stringColorStorage = new ColorStorage<string>();
+stringColorStorage.addColor("red");
+stringColorStorage.addColor("green");
+stringColorStorage.addColor("blue");
+
+console.log(stringColorStorage.getAllColors());
+
+class DataStorage<T> {
   private items: T[] = [];
 
   add(item: T): void {
     this.items.push(item);
   }
-
-  getAll(): T[] {
+  getAllItems(): T[] {
     return [...this.items];
   }
 }
 
-type Product = {
-  id: number;
-  name: string;
-  price: number;
+class NumberStorageWithSum extends DataStorage<number> {
+  sum(): number {
+    return this.getAllItems().reduce((acc, cur) => acc + cur);
+  }
+}
+
+let testData = new NumberStorageWithSum();
+testData.add(5);
+testData.add(10);
+testData.add(15);
+console.log(testData.sum());
+
+class HistoryStorage<T> {
+  private items: T[] = [];
+  private history: T[] = [];
+
+  add(item: T): void {
+    this.items.push(item);
+    this.history.push(item);
+  }
+  getAllItems(): T[] {
+    return [...this.items];
+  }
+  getHistory(): T[] {
+    return [...this.history];
+  }
+  remove(item: T) {
+    this.items = this.items.filter((element) => element !== item);
+  }
+}
+
+const hs = new HistoryStorage<string>();
+hs.add("A");
+hs.add("B");
+hs.remove("A");
+hs.add("C");
+
+console.log(hs.getAllItems()); // ["B", "C"]
+console.log(hs.getHistory());
+
+interface Pair<T> {
+  first: T;
+  second: T;
+}
+
+function swap<T>(pair: Pair<T>): Pair<T> {
+  return { first: pair.second, second: pair.first };
+}
+
+interface Container<T> {
+  addItem(item: T): void;
+  getItem(): T;
+}
+
+class Box<T> implements Container<T> {
+  private item!: T;
+  addItem(item: T): void {
+    this.item = item;
+  }
+
+  getItem(): T {
+    return this.item;
+  }
+}
+
+interface ApiResponse<T> {
+  status: string;
+  data: T;
+}
+
+class ApiWrapper<T> {
+  private response: ApiResponse<T>;
+  constructor(response: ApiResponse<T>) {
+    this.response = response;
+  }
+
+  getDate(): T {
+    return this.response.data;
+  }
+
+  isSuccess(): boolean {
+    return this.response.status === "success";
+  }
+}
+
+const response: ApiResponse<string> = {
+  status: "success",
+  data: "Hello from API",
 };
 
-function getProp<T, K extends keyof T>(item: T, key: K) {
-  return item[key];
-}
-const item: Product = {
-  id: 1,
-  name: "Keyboard",
-  price: 3000,
-};
-
-const result = getProp(item, "name");
-
-function average<T extends number[]>(values: T): number {
-  // let result = 0;
-  // values.forEach((value) => (result += value));
-  // result = result / values.length;
-  // return result;
-
-  const sum = values.reduce((acc, val) => acc + val, 0);
-
-  return sum / values.length;
-}
+const wrapper = new ApiWrapper(response);
+console.log();
