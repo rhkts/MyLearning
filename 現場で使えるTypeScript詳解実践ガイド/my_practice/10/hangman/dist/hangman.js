fff46d9d0f63ba98dd2375bbbdc3583279fa96b4
rhkts <tahagoo168@gmail.com>
2025-04-18 15:15:12 +0900

    hangman 出題する単語とヒントデータを作成

--- 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/dist/hangman.js
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/dist/hangman.js
@@ -1,3 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const question_test_json_1 = __importDefault(require("./data/question.test.json"));
 console.log("Game Start!");
+const questions = question_test_json_1.default;
+console.log(questions);
--- 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/src/hangman.ts
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/src/hangman.ts
@@ -1,1 +1,12 @@
+import rawData from "./data/question.test.json";
+
 console.log("Game Start!");
+
+interface Question {
+  word: string;
+  hint: string;
+}
+
+const questions: Question[] = rawData;
+
+console.log(questions);
