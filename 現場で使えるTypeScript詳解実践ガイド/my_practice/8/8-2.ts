663af0184f41f77350b27fb39e97eef0989e035b
rhkts <tahagoo168@gmail.com>
2025-04-18 14:40:40 +0900

    8-2 ts-odd追加

--- 現場で使えるTypeScript詳解実践ガイド/my_practice/8/8-2.ts
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/8/8-2.ts
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import isOdd from "is-odd";
 
 const list = [1, 2, 3, 4, 5];
 const strList = ["あ", "い", "う"];
@@ -6,3 +7,6 @@ const strList = ["あ", "い", "う"];
 const shuffled = _.shuffle(strList);
 
 console.log(shuffled);
+
+console.log(isOdd(2));
+console.log(isOdd(3));
