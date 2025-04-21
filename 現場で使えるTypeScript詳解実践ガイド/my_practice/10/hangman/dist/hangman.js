eff98ebefef8af6206e78fb457cb6694533694ff
rhkts <tahagoo168@gmail.com>
2025-04-21 13:35:41 +0900

    hangman 動作確認用のインターフェイスとメソッドを追加

--- 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/dist/hangman.js
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/dist/hangman.js
@@ -4,21 +4,57 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const question_test_json_1 = __importDefault(require("./data/question.test.json"));
+const promises_1 = __importDefault(require("readline/promises"));
 console.log("Game Start!");
-const questions = question_test_json_1.default;
-// console.log(questions);
 class Quiz {
     questions;
     constructor(questions) {
         this.questions = questions;
     }
+    //次の問題が存在するか確認
+    hasNext() {
+        return this.questions.length > 0;
+    }
     //ランダムに質問を取得してその質問をリストから削除
     getNext() {
         //0以上、配列の長さ以下のランダムな整数を生成
         const idx = Math.floor(Math.random() * this.questions.length);
+        //ランダムなidxを使って、quwstionsから1つの問題を削除し変数questionに代入
         const [question] = this.questions.splice(idx, 1);
-        return [question];
+        return question;
+    }
+    //残りの問題数を取得
+    lefts() {
+        return this.questions.length;
     }
 }
+const questions = question_test_json_1.default;
 const quiz = new Quiz(questions);
+//readlinePromisesインターフェイスのインスタンス(実体)を生成
+const rl = promises_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const CLI = {
+    //プロパティとメソッドを追加する
+    async input() {
+        const input = await rl.question("文字または単語を推測してください：");
+        return input.replaceAll(" ", "").toLocaleLowerCase();
+    },
+    clear() {
+        console.clear();
+    },
+    destroy() {
+        console.log("終了します");
+        rl.close();
+    },
+};
 console.log(quiz.getNext());
+//動作確認用
+async function testQuestion() {
+    CLI.clear(); //画面をクリア
+    const userInput = await CLI.input(); //入力を受け付けて返す。
+    console.log(userInput);
+    CLI.destroy(); //セッション終了
+}
+testQuestion();
--- 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/src/hangman.ts
+++ 現場で使えるTypeScript詳解実践ガイド/my_practice/10/hangman/src/hangman.ts
@@ -1,4 +1,5 @@
 import rawData from "./data/question.test.json";
+import readlinePromise from "readline/promises";
 
 console.log("Game Start!");
 
@@ -7,8 +8,13 @@ interface Question {
   hint: string;
 }
 
-const questions: Question[] = rawData;
-// console.log(questions);
+interface UserIntarfase {
+  input(): Promise<string>;
+  clear(): void;
+  destroy(): void;
+  // output(message: string, color?: Color): void;
+  // outputAnswer(message: string): void;
+}
 
 class Quiz {
   questions: Question[];
@@ -16,16 +22,60 @@ class Quiz {
     this.questions = questions;
   }
 
+  //次の問題が存在するか確認
+  hasNext(): boolean {
+    return this.questions.length > 0;
+  }
+
   //ランダムに質問を取得してその質問をリストから削除
-  getNext(): Question[] {
+  getNext(): Question {
     //0以上、配列の長さ以下のランダムな整数を生成
     const idx = Math.floor(Math.random() * this.questions.length);
-
+    //ランダムなidxを使って、quwstionsから1つの問題を削除し変数questionに代入
     const [question] = this.questions.splice(idx, 1);
 
-    return [question];
+    return question;
+  }
+
+  //残りの問題数を取得
+  lefts(): number {
+    return this.questions.length;
   }
 }
 
+const questions: Question[] = rawData;
 const quiz = new Quiz(questions);
+
+//readlinePromisesインターフェイスのインスタンス(実体)を生成
+const rl = readlinePromise.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const CLI: UserIntarfase = {
+  //プロパティとメソッドを追加する
+  async input() {
+    const input = await rl.question("文字または単語を推測してください：");
+    return input.replaceAll(" ", "").toLocaleLowerCase();
+  },
+
+  clear() {
+    console.clear();
+  },
+  destroy() {
+    console.log("終了します");
+    rl.close();
+  },
+};
+
 console.log(quiz.getNext());
+
+//動作確認用
+async function testQuestion() {
+  CLI.clear(); //画面をクリア
+  const userInput = await CLI.input(); //入力を受け付けて返す。
+  console.log(userInput);
+  CLI.destroy(); //セッション終了
+}
+
+testQuestion();
