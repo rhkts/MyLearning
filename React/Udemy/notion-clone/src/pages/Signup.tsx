4f06da3accd51ed6b8edda262a7e6e90b2472a23
rhkts <tahagoo168@gmail.com>
2025-04-28 14:17:00 +0900

    notion ユーザー登録機能を追加

--- React/Udemy/notion-clone/src/pages/Signup.tsx
+++ React/Udemy/notion-clone/src/pages/Signup.tsx
@@ -1,4 +1,28 @@
+import { authRepository } from "@/modules/auth/auth.repositpry";
+import { ChangeEventHandler, useState } from "react";
+
 function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const signup = async () => {
+    const user = await authRepository.signup(name, email, password);
+    console.log(user);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center">
@@ -17,6 +41,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={onChangeName}
                     id="username"
                     name="username"
                     placeholder="ユーザー名"
@@ -35,6 +60,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={onChangeEmail}
                     id="email"
                     name="email"
                     placeholder="メールアドレス"
@@ -53,6 +79,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={onChangePassword}
                     id="password"
                     name="password"
                     placeholder="パスワード"
@@ -63,7 +90,11 @@ function Signup() {
                 </div>
               </div>
               <div>
-                <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 disabled:opacity-50 disabled:cursor-not-allowed">
+                <button
+                  disabled={name === "" || email === "" || password === ""}
+                  onClick={signup}
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
                   登録
                 </button>
               </div>
