db3fe2152f86b49812eebe589e67a96662c8900c
rhkts <tahagoo168@gmail.com>
2025-04-28 17:10:48 +0900

    notion 一部修正

--- React/Udemy/notion-clone/src/pages/Signin.tsx
+++ React/Udemy/notion-clone/src/pages/Signin.tsx
@@ -23,7 +23,7 @@ function Signin() {
     currentUSerStore.set(user);
   };
 
-  if (currentUSerStore.currentUser !== null) return <Navigate to="/" replace />;
+  if (currentUSerStore.currentUser != null) return <Navigate to="/" replace />;
 
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
