a9629232761362922f62175f60c7afee0af80e6e
rhkts <tahagoo168@gmail.com>
2025-04-30 14:50:46 +0900

    zenn 画像更新

--- React/Udemy/notion-clone/src/modules/auth/current-user.state.ts
+++ React/Udemy/notion-clone/src/modules/auth/current-user.state.ts
@@ -2,7 +2,6 @@ import { User } from "@supabase/supabase-js";
 import { atom, useAtom } from "jotai";
 
 const CurrentUserAtom = atom<User>();
-
 export const useCurrentUserStore = () => {
   const [currentUser, setCurrentUser] = useAtom(CurrentUserAtom);
 
