7bf4966598a753a39bd1132a815c8c71890a58bb
rhkts <tahagoo168@gmail.com>
2025-05-01 13:14:06 +0900

    notion NoteStateを追加

---
+++ React/Udemy/notion-clone/src/modules/notes/note.state.ts
@@ -0,0 +1,29 @@
+import { atom, useAtom } from "jotai";
+import { Note } from "./note.entity";
+
+//atomを用意
+const noteAtom = atom<Note[]>([]);
+
+//atomを操作するフックを用意
+export const usetNoteStore = () => {
+  const [note, setNote] = useAtom(noteAtom);
+
+  const set = (newNotes: Note[]) => {
+    setNote((oldNotes) => {
+      const combineNotes = [...oldNotes, ...newNotes];
+
+      const uniqueNotes: { [key: number]: Note } = {};
+
+      for (const note of combineNotes) {
+        uniqueNotes[note.id] = note;
+      }
+
+      return Object.values(uniqueNotes);
+    });
+  };
+
+  return {
+    getAll: () => note,
+    set,
+  };
+};
