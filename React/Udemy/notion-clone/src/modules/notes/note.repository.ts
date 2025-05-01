0aa0be893ee7a12675ab07c77ea4659c3179e018
rhkts <tahagoo168@gmail.com>
2025-05-01 14:08:44 +0900

    notion findメソッドを追加

--- React/Udemy/notion-clone/src/modules/notes/note.repository.ts
+++ React/Udemy/notion-clone/src/modules/notes/note.repository.ts
@@ -18,4 +18,28 @@ export const noteRepository = {
 
     return data;
   },
+
+  async find(userId: string, parentDocumentId: number) {
+    const query = supabase
+      .from("notes")
+      .select()
+      .eq("user_id", userId)
+      .order("created_at", { ascending: false });
+
+    //ここでデータ取得
+    // console.log("----");
+    // console.log((await query).data);
+    // console.log("----");
+
+    const { data } =
+      parentDocumentId != null
+        ? await query.eq("parent_document", parentDocumentId)
+        : await query.is("parent_document", null);
+
+    //取得したデータの中から絞り込み
+    // console.log("====");
+    // console.log(data);
+    // console.log("====");
+    return data;
+  },
 };
